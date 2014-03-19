92d09651c27238652df0bfed422d9436bfbb7727
MacBoyPro <lee.lundrigan@gmail.com>
2014-03-18 23:17:25 -0700

    added first user spec for password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,28 @@ class User
   include Neo4j::ActiveNode
   extend ::Devise::Models
   extend ::Devise::Orm::Neo4j::Hook
+
+  ##############
+  ### Devise ###
+  ##############
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  ##################
+  ### Properties ###
+  ##################
+
   property :email, :index => :exact, :unique => true
   property :encrypted_password
+  
+
+  ###################
+  ### Validations ###
+  ###################
+
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should require a password" do
+    expect(User.new(password: "")).not_to be_valid
+  end
+
 end
